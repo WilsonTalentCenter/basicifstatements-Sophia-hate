ed94303db87c1de34e3fa84092926d67fe97a5c1
Sophia-hate <sophiamukambilwa8@gmail.com>
2024-10-08 13:46:48 -0400

    ujghf

--- Setup/Main.java
+++ Setup/Main.java
@@ -20,17 +20,17 @@ public class Main {
 		
 		//write an if statement that prints out "Why did the man fall down the well?" if the input was less than 5
         if (userInput1 < 5) {
-            System.out.println("Why did the man fall down well?");
+            System.out.println("Why did the man fall down the well?");
         }
 		
 		//write an if statement that prints out "Because he couldn't see that well." if the input was greater than 1
         if (userInput1 > 1) {
-            System.out.println("Because he couldn't see that well");
+            System.out.println("Because he couldn't see that well.");
         }
 		
 		//write an if statement that prints out "When does a joke become a dad joke?"  if the input was equal to 8
         if (userInput1 == 8) {
-            System.out.println("When does a joke become a dad joke");
+            System.out.println("When does a joke become a dad joke?");
         }
 		
 		//write an if statement that prints out "When it becomes apparent."  if the input was greater than or equal to 8
@@ -40,12 +40,12 @@ public class Main {
 		
 		//write an if statement that prints out "I used to hate facial hair," if the input is less than or equal to 12
         if (userInput1 < 12 || userInput1 == 12){
-            System.out.println("I used to hate facial hair");
+            System.out.println("I used to hate facial hair,");
         }
 		
 		//write an if statement that prints out " but then it grew on me."  if the input does not equal 23
         if (userInput1 != 23) {
-            System.out.println(" but then it grew on me");
+            System.out.println(" but then it grew on me.");
         }
 		
 	}
