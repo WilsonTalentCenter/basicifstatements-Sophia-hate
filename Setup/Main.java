8233ae16e95b96f4765f87bf57d0bcabf5cb747d
Sophia-hate <sophiamukambilwa8@gmail.com>
2024-10-08 13:42:27 -0400

    jkbghgh

--- Setup/Main.java
+++ Setup/Main.java
@@ -11,21 +11,42 @@ public class Main {
         System.out.println("enter a whole number");
 		
 		//get a line of input convert and save the input into an integer variable (you can assume the user enters a  valid whole number)
-        
+        int userInput1 = Integer.parseInt(scanner.nextLine());
 		
 		//write an if statement that prints "hello world" if the input was less than 4
+        if (userInput1 < 4) {
+            System.out.println("hello world");
+        }
 		
 		//write an if statement that prints out "Why did the man fall down the well?" if the input was less than 5
+        if (userInput1 < 5) {
+            System.out.println("Why did the man fall down well?");
+        }
 		
 		//write an if statement that prints out "Because he couldn't see that well." if the input was greater than 1
+        if (userInput1 > 1) {
+            System.out.println("Because he couldn't see that well");
+        }
 		
 		//write an if statement that prints out "When does a joke become a dad joke?"  if the input was equal to 8
+        if (userInput1 == 8) {
+            System.out.println("When does a joke become a dad joke");
+        }
 		
 		//write an if statement that prints out "When it becomes apparent."  if the input was greater than or equal to 8
+        if (userInput1 > 8 || userInput1 == 8) {
+            System.out.println("When it becomes apparent.");
+        }
 		
 		//write an if statement that prints out "I used to hate facial hair," if the input is less than or equal to 12
+        if (userInput1 < 12 || userInput1 == 12){
+            System.out.println("I used to hate facial hair");
+        }
 		
 		//write an if statement that prints out " but then it grew on me."  if the input does not equal 23
+        if (userInput1 != 23) {
+            System.out.println(" but then it grew on me");
+        }
 		
 	}
 
